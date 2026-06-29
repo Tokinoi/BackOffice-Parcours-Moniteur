973a938a28e8b4bcda3d5472fe94deb0778840dc
Tokinoi <93129440+Tokinoi@users.noreply.github.com>
2026-06-29 10:49:06 +0200

    Change constants
    Added new POI types including 'Grand Parking calme', 'Rue calme', and others.

--- src/constants/poiOptions.js
+++ src/constants/poiOptions.js
@@ -1,10 +1,14 @@
 // src/constants/poiOptions.js
 
 export const POI_TYPES = [
-    { value: "vadrouille", label: "Vadrouille" },
-    { value: "danger", label: "Danger" },
-    { value: "interet", label: "Point d’intérêt" },
-    { value: "parking", label: "Parking" },
+    { value: "Grand Parking calme", label: "Grand Parking calme" },
+    { value: "Rue calme", label: "Rue calme" },
+    { value: "Quartier/Lotissement", label: "Quartier/Lotissement" },
+    { value: "Priorité à droite", label: "Priorité à droite" },
+    { value: "Giratoire", label: "Giratoire" },
+    { value: "Côte", label: "Côte" },
+    { value: "Rue étroite", label: "Rue étroite" },
+    { value: "Feu à l'indonésienne", label: "Feu à l'indonésienne" },
 ];
 
 export const POI_STATUSES = [
